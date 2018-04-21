491fbb4732552217753aa48c4899046db6919075
Matt Katz <git@morelightmorelight.com>
2018-04-20 21:43:05 -0400

    fixed home test, set up better framework

--- tests/test_voracious.py
+++ tests/test_voracious.py
@@ -4,8 +4,12 @@ import context
 import voracious.app
 
 
+def client():
+    return voracious.app.test_client()
+
 def test_tests_are_working():
     assert 1 == 1
 
-def test_home_says_hello():
-    assert home() == "hello"
+def test_home_says_hello(client):
+    rv = client.get('/')
+    assert rv.data  == "hello"
