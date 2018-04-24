43417ca353e6b4a1b9d87c22ff1bd2d22f983aa2
Matt Katz <git@morelightmorelight.com>
2018-04-24 09:28:42 -0400

    use proper flask testing pattern
    flask comes wiht a test client, so lets use it!

--- tests/test_voracious.py
+++ tests/test_voracious.py
@@ -1,15 +1,19 @@
 import os
 import tempfile
 import context
-import voracious.app
+import pytest
+from voracious.app import app
 
 
+@pytest.fixture
 def client():
-    return voracious.app.test_client()
+    return app.test_client()
+
 
 def test_tests_are_working():
     assert 1 == 1
 
+
 def test_home_says_hello(client):
     rv = client.get('/')
-    assert rv.data  == "hello"
+    assert rv.data == b"hello"
