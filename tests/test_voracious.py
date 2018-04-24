b554e17cd55fe084a8ef222134a2d451d6bc8eaf
Matt Katz <git@morelightmorelight.com>
2018-04-24 15:49:27 -0400

    test the api lists models

--- tests/test_voracious.py
+++ tests/test_voracious.py
@@ -1,13 +1,15 @@
-import os
-import tempfile
 import context
 import pytest
 from voracious.app import app
+from unittest.mock import patch
+from tests.dummy_models.dummies import Dumb, Dumber
 
 
 @pytest.fixture
 def client():
-    return app.test_client()
+    client = app.test_client()
+    client.testing = True
+    return client
 
 
 def test_tests_are_working():
@@ -17,3 +19,13 @@ def test_tests_are_working():
 def test_home_says_hello(client):
     rv = client.get('/')
     assert rv.data == b"hello"
+
+
+def test_api_index(client):
+    with patch('voracious.app.em') as mock_em:
+        mock_em.models = []
+        mock_em.models.append(Dumb)
+        mock_em.models.append(Dumber)
+        rv = client.get('/api/models/')
+        assert b'Dumb' in rv.data
+        assert b'Dumber' in rv.data
--- voracious/app.py
+++ voracious/app.py
@@ -1,19 +1,19 @@
 '''This is the app that will provice a web api on top of all the different
    members
 '''
+from voracious.entitymanager import EntityManager
 import os
 import sys
 from pathlib import Path
-from flask import Flask
+from flask import Flask, jsonify
 sys.path.append(Path(__file__).parent)
-from voracious.entitymanager import EntityManager
 sys.path.append('..')
 sys.path.append('../models')
 
 app = Flask(__name__)
 print(os.getcwd())
 mp = Path(__file__) / '..'/ '..' / 'models'
-em  = EntityManager(models_path=mp.resolve())
+em = EntityManager(models_path=mp.resolve())
 
 
 
@@ -22,16 +22,16 @@ def home():
     return "hello"
 
 # TODO get all the models listed in models
-@app.route('/index')
+@app.route('/api/models/')
 def model_index():
     '''Return all the models supported'''
-    resp = f'We have {len(em.models)} models'
-    model_list = ''.join([f'<li>{model.__name__}</li>' for model in em.models])
-    print(model_list)
-    resp += f'<ul>{model_list}</ul>'
-    return resp
+    model_list = [model.__name__ for model in em.models]
+    return jsonify({'entities': model_list})
 
     # TODO for each model, get all the properties and attributes of the module. 
     # TODO maybe we should get all the ones decorated with some decorator?
     # TODO or should we just ignore only ones decorated with a decorator?
     # 
+
+if __name__ == '__main__':
+    app.run(debug=true)
